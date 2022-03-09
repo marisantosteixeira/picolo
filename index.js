03c75df3d5039a042536400e6388c69f0b887677
MarianaTeixeir4 <>
2022-03-09 18:44:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+class Retangulo{ 
+  constructor(altura, largura){ 
+    this.altura = altura;
+    this.largura = largura;
+  }
+
+   calcularArea() {
+    return this.altura * this.largura;
+
+  }
+}
+let retangulo = new Retangulo("10", "10");
+let retangule = new Retangulo("30", "60");
+
+console.log(retangulo.calcularArea())
+console.log(retangule.calcularArea())
+
+
+
+
+
